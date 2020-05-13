87660c318d6ed512e8439d6d7099e9868040c0ca
Aniket Mishra <ianiket23@gmail.com>
2020-05-13 01:40:05 -0600

    Add files via upload

---
+++ Assests/Camera/CamSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CamSwitch : MonoBehaviour
+{
+    public CinemachineVirtualCamera vCam;
+    public CinemachineConfiner conf;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Trigger");
+        if(other.transform.name == "Player")
+        {
+            conf.m_BoundingShape2D = this.GetComponent<Collider2D>();
+            Debug.Log("Player");
+        }
+    }
+}
